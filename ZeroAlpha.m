8093bdd9e444a970f133ffac8385cfca3d9e918e
LuigiPolizio <68841456+LuigiPolizio@users.noreply.github.com>
2020-07-27 21:34:23 +0200

    Delete ZeroAlpha.m

--- ZeroAlpha.m
+++
@@ -1,5 +0,0 @@
-function CL_Diff = ZeroAlpha(alpha,CL_ASS,delta_e,V,q)
-global sound_speed_0 delta_s_0 myAC
-[CL_AC,~,~,~,~] = AeroCoeff(alpha*180/pi,delta_e*180/pi,V/sound_speed_0,0,q*180/pi,V);
-CL_Diff=CL_ASS-(CL_AC+myAC.CL_delta_s*delta_s_0);
-end
